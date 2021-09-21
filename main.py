7e9e614796612ab2a5277fc45187bbfd31916c18
CustomeryXD <vladpoligalov3@gmail.com>
2021-09-21 18:12:31 +0300

    two

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # aiogram
 import logging
+import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, executor, types
-# weather
-import pyowm
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ParseMode
+from aiogram.utils import executor
 # data base
 import sqlite3
 import threading
@@ -12,6 +17,11 @@ import phrases
 import time
 # config
 import config as cfg
+import sys
+sys.path.insert(0, sys.path[0] + '/parser')
+from parse import *
+
+
 
 
 bot = Bot(cfg.TOKEN)
@@ -27,6 +37,33 @@ async def send_start_message(message: types.Message):
 @dp.message_handler(commands=['help', 'hel', 'he', 'h'])
 async def send_help_message(message: types.Message):
     await bot.send_message(message.chat.id, phrases.help_Message)
+
+@dp.message_handler(content_types=['text'])
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def request_anime_title(message: types.Message):
+    if ('/add' in str(message.text).lower()):
+        title = message.text.split(sep=" ", maxsplit = 1)
+        anime_list = parse(title[1])
+        anime_list_string = ''
+        i = 1
+        for anime in anime_list:
+            anime_list_string += str(i)+') '+anime['name'] + '\n'
+            i+=1
+
+
+        await bot.send_message(message.chat.id,anime_list_string)
+        send = bot.send_message(message.chat.id, 'Введите номер аниме ')
+        bot.register_next_step_handler(send, number_anime)
+
+
+        
+
+        number = int(message.text)
+        await bot.send_message(message.chat.id,number)
+
+        
+
+
 
 
 # приветствие
