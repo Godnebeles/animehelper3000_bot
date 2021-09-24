9eed24cf3fb0d59afc61d4a972bdae8bd6940cb7
CustomeryXD <vladpoligalov3@gmail.com>
2021-09-24 21:02:26 +0300

    four

--- WorkClasses/add_anime.py
+++ WorkClasses/add_anime.py
@@ -10,20 +10,23 @@ from aiogram.utils import executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from YummyParser.parse import *
+import databasemanager as db
+
+db.create_data_base()
 
 class request_anime(StatesGroup):
     waiting_title = State()
     waiting_number_title = State()
     anime_titles = []
+    found_anime = {}
 
     async def request_anime_title(bot, message):
         if ('/add' in str(message.text).lower()):
             title = message.text.split(sep=" ", maxsplit = 1)
-            anime_list = parse(title[1])
-            request_anime.anime_titles = anime_list
+            request_anime.anime_titles = parse(title[1])
             anime_list_string = ''
             i = 1
-            for anime in anime_list:
+            for anime in request_anime.anime_titles:
                 anime_list_string += str(i)+') '+anime['name'] + '\n'
                 i+=1
 
@@ -34,8 +37,11 @@ class request_anime(StatesGroup):
    
     async def get_number_title(bot,message, state):
         number = int(message.text)
-        anime_list = request_anime.anime_titles
-        curr_anime = anime_list[number-1]["name"]
+        curr_anime = request_anime.anime_titles[number-1]["name"]
+        request_anime.found_anime = request_anime.anime_titles[number-1]
         await bot.send_message(message.chat.id, f"Отлично, будем отслеживать:\n{curr_anime}")
+        db.data_base_updater(message, curr_anime, anime_parse(request_anime.found_anime["alias"]))
         await state.finish()
+        
+        
 
--- YummyParser/parse.py
+++ YummyParser/parse.py
@@ -11,9 +11,10 @@ from bs4 import BeautifulSoup
 
 
 
-#title = input("Введите название фильма: ")
+
 #search = "https://yummyanime.club/get-search-list?&word=" + title + "&page=1"
-link = " https://yummyanime.club/get-search-list?&word="# + urllib.parse.quote("Домекано")
+search_link = " https://yummyanime.club/get-search-list?&word="# + urllib.parse.quote("Домекано")
+current_anime_link = "https://yummyanime.club/catalog/item/"
 
 HEADERS = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
 
@@ -28,18 +29,28 @@ def get_html(url, params=None):
     return urlopen(response).read()
 
 
-def get_content(html):
+def get_search_content(html):
     soup = BeautifulSoup(html, 'html.parser') #, params=params
     #items = soup.find('table', class_='tbl-switcher').find('tbody').find_all('tr')[0].find_all('td')[5].text
     items = soup.find('div', class_='content-page') #.find_all('div', class_='anime-column')
     ourjJson = json.loads(str(soup))
     return ourjJson['animes']['data']
 
+def get_current_anime_data(link):
+    soup = BeautifulSoup(get_html(link), 'html.parser')
+    items = soup.find('div', class_="block-episodes").find_all('div')
+    return len(items)
+
 
 def parse(title):
-    html = get_html(link + urllib.parse.quote(title))
+    html = get_html(search_link + urllib.parse.quote(title))
     #if html.status_code == 200:
-    return get_content(html)
+    return get_search_content(html)
     #else:
         # print(search)
         # print('Error: ' + str(html.status_code))
+
+def anime_parse(alias):
+    link = current_anime_link + alias
+    return get_current_anime_data(link)
+    
